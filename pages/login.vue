24c5e3411cbaa25c4212cf9191ecc170ac3e7d52
Guilherme Lino <lino000000@hotmail.com>
2023-11-28 17:20:31 +0000

    change from vuetify to nuxt-UI

--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,33 @@
 <template>
-    <div class="d-flex align-center justify-center" style="height: 100vh">
-            <v-card class="mx-auto pa-12" width="50%" >
-                <div class="d-flex align-center justify-center">
-                    <v-sheet width="400" class="mx-auto">
-                        <v-form fast-fail @submit.prevent=" ">
-                            <v-text-field label="User Name"></v-text-field>
+  <div class="d-flex align-center justify-center" style="height: 100vh">
+    <UCard>
+      <template #header>
+        <Placeholder class="h-8" />
+      </template>
 
-                            <v-text-field label="password"></v-text-field>
-                            <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
+      <Placeholder class="h-32" />
+      <div class="d-flex align-center justify-center">
+        <v-sheet width="400" class="mx-auto">
+          <v-form fast-fail @submit.prevent=" ">
+            <label class="text-body-2 font-weight-regular">Username</label>
+            <UInput placeholder="Username"></UInput>
+            <label class="text-body-2 font-weight-regular">Password</label>
+            <UInput placeholder="Password"></UInput>
+            <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
 
-                            <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
+            <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
 
-                        </v-form>
-                        <div class="mt-2">
-                            <p class="text-body-2">Don't have an account? <a href="#">Sign Up</a></p>
-                        </div>
-                    </v-sheet>
-                </div>
-            </v-card>
-    </div>
+          </v-form>
+          <div class="mt-2">
+            <p class="text-body-2">Don't have an account? <a href="#">Sign Up</a></p>
+          </div>
+        </v-sheet>
+      </div>
+
+      <template #footer>
+        <Placeholder class="h-8" />
+      </template>
+    </UCard>
+  </div>
 </template>
+
