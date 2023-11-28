6dc1d6e2fd1aa8038b722717e97bac10019e061f
Guilherme Lino <lino000000@hotmail.com>
2023-11-28 13:36:18 +0000

    Initial commit

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card class="mx-auto"  width="50%">
+        <div class="d-flex align-center justify-center" >
+            <v-sheet width="400" class="mx-auto">
+                <v-form fast-fail @submit.prevent=" ">
+                    <v-text-field label="User Name"></v-text-field>
+
+                    <v-text-field label="password"></v-text-field>
+                    <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
+
+                    <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
+
+                </v-form>
+                <div class="mt-2">
+                    <p class="text-body-2">Don't have an account? <a href="#">Sign Up</a></p>
+                </div>
+            </v-sheet>
+        </div>
+    </v-card>
+</template>
