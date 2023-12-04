baac01062877e516d4528280c3c06845322c1f55
Guilherme Lino <lino000000@hotmail.com>
2023-12-04 19:41:41 +0000

    Revert "change from vuetify to nuxt-UI"
    This reverts commit 24c5e3411cbaa25c4212cf9191ecc170ac3e7d52.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify'],
+  }
 })
