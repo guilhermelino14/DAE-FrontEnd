d4559bb380aaf4a1f4a0d2a0d8a1df6f0ab13eb9
Guilherme Lino <lino000000@hotmail.com>
2023-12-05 13:14:17 +0000

    add navbar

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="flex flex-col items-center justify-center px-6 pt-8 mx-auto md:h-screen pt:mt-0 ">
+<div class="flex flex-col items-center justify-center px-6 mx-auto md:h-screen pt:mt-0 ">
     <!-- Card -->
     <div class="w-full max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow dark:bg-gray-800">
         <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
