d188bcb088f912e83bbb4d5fb877a1f40f9c8c68
Guilherme Lino <lino000000@hotmail.com>
2023-12-05 16:05:49 +0000

    upsi

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,23 +11,6 @@ onMounted(() => {
 <template>
   <div>
 
-<<<<<<< Updated upstream
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="./" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="logoWhite.png" class="h-8" alt="Logo" />
-  </a>
-  <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <span class="sr-only">Open user menu</span>
-        <img class="w-8 h-8 rounded-full" src="https://www.svgrepo.com/download/43426/profile.svg" alt="user photo">
-      </button>
-      <!-- Dropdown menu -->
-      <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-        <div class="px-4 py-3">
-          <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-          <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-=======
 
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -79,7 +62,6 @@ onMounted(() => {
                 d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
->>>>>>> Stashed changes
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
           <ul
