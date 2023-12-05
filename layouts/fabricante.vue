816b7304387faebb5face338464c837f4b225495
Guilherme Lino <lino000000@hotmail.com>
2023-12-05 14:22:47 +0000

    add the layout of fabricante

---
+++ layouts/fabricante.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <div>
+    <NavBar></NavBar>
+    <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
+      <Sidebar></Sidebar>
+      <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+        <main>
+          <slot />
+        </main>
+        <!-- footer -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Sidebar from '../components/fabricanteLayout/sidebar.vue'
+import NavBar from '../components/fabricanteLayout/topBar.vue'
+onMounted(() => {
+  initFlowbite();
+})
+</script>
