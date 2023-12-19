fcab47917ac8f5bdfe18e49d32b4a33a39f3f327
Guilherme Lino <lino000000@hotmail.com>
2023-12-19 14:23:07 +0000

    add sidebar mobile

--- components/fabricanteLayout/topBar.vue
+++ components/fabricanteLayout/topBar.vue
@@ -3,7 +3,7 @@
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
       <div class="flex items-center justify-between">
         <div class="flex items-center justify-start">
-          <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar"
+          <button @click="mobile = !mobile" id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar"
             class="p-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
             <svg id="toggleSidebarMobileHamburger" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
@@ -355,28 +355,11 @@
             </svg>
             Login/Register
           </a>
-          <button data-collapse-toggle="mobile-menu" type="button"
-            class="inline-flex items-center justify-center p-2 ml-3 text-gray-400 rounded-lg sm:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-300 dark:hover:bg-gray-700 dark:hover:text-white"
-            aria-controls="mobile-menu-2" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <!-- Open mobile menu icon -->
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <!-- Close mobile menu icon -->
-            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"></path>
-            </svg>
-          </button>
         </div>
       </div>
     </div>
     <!-- Mobile menu -->
-    <div class="hidden sm:hidden" id="mobile-menu">
+    <div v-show="mobile">
       <ul class="pt-2">
         <li>
           <a href="#"
@@ -411,12 +394,12 @@
   </nav>
 </template>
 <script setup>
-const login = true
-
 import { onMounted, ref, computed } from 'vue'
 import { initFlowbite } from 'flowbite'
 import { useAuthStore } from '../../store/auth-store'
 
+const login = ref(true)
+const mobile = ref(false)
 
 const authStore = useAuthStore()
 // initialize components based on data attribute selectors
