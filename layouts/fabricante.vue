c4a9c1ea21af8168184285828aae3cea8e9c4ef2
Guilherme Lino <lino000000@hotmail.com>
2023-12-22 15:45:07 +0000

    update height

--- layouts/fabricante.vue
+++ layouts/fabricante.vue
@@ -4,7 +4,7 @@
     <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
       <Sidebar></Sidebar>
       <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
-        <main>
+        <main style="height: 100vh;">
           <div class="px-4 pt-6">
             <slot />
           </div>
