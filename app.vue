eb04d64198ac79708f96e9e447aaa547165ae85b
Guilherme Lino <lino000000@hotmail.com>
2023-12-22 15:34:10 +0000

    validate token martelado

--- app.vue
+++ app.vue
@@ -5,5 +5,21 @@
 </template>
 
 <script setup>
+import { useAuthStore } from "~/store/auth-store.js"
+const authStore = useAuthStore()
+const config = useRuntimeConfig()
+const api = config.public.API_URL
 
+onMounted(async () => {
+  if (authStore.token != "") {
+    const resposnse = await useFetch(`${api}/auth/token/` + authStore.token)
+    if(resposnse.status.value == "success"){
+      return
+    }
+    if (resposnse.error.value.statusCode == 500) {
+      authStore.logout()
+      navigateTo("/login")
+    }
+  }
+})
 </script>
