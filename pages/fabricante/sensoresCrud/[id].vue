859fff22f4da75c8dc42bde95aaa0567e5456be2
Guilherme Lino <lino000000@hotmail.com>
2023-12-23 16:32:58 +0000

    add observacoes to sensor Fabricante

--- pages/fabricante/sensoresCrud/[id].vue
+++ pages/fabricante/sensoresCrud/[id].vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800 pb-2 mb-3">
+    <div
+        class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800 pb-2 mb-3">
         <NuxtLink to="/fabricante/sensores">
             <button type="button"
                 class="mb-2 py-1.5 px-3 inline-flex items-center rounded-lg bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700">
@@ -24,22 +25,21 @@
         <div class="flow-root">
             <h3 class="text-xl font-semibold dark:text-white">Observações</h3>
             <ul class="divide-y divide-gray-200 dark:divide-gray-700">
-                <li class="py-4">
+                <li class="pt-4 pb-6" v-for="observacao in sensor.observacoes">
                     <div class="flex items-center space-x-4">
                         <div class="flex-shrink-0">
-                            <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-                                </path>
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
+                                <path
+                                    d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
                             </svg>
                         </div>
                         <div class="flex-1 min-w-0">
                             <p class="text-base font-semibold text-gray-900 truncate dark:text-white">
-                                California 123.123.123.123
+                                {{ observacao.observacao }}
                             </p>
                             <p class="text-sm font-normal text-gray-500 truncate dark:text-gray-400">
-                                Chrome on macOS
+                                {{ observacao.data }}
                             </p>
                         </div>
                         <div class="inline-flex items-center">
@@ -48,31 +48,11 @@
                         </div>
                     </div>
                 </li>
-                <li class="pt-4 pb-6">
-                    <div class="flex items-center space-x-4">
-                        <div class="flex-shrink-0">
-                            <svg class="w-6 h-6 dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"></path>
-                            </svg>
-                        </div>
-                        <div class="flex-1 min-w-0">
-                            <p class="text-base font-semibold text-gray-900 truncate dark:text-white">
-                                Rome 24.456.355.98
-                            </p>
-                            <p class="text-sm font-normal text-gray-500 truncate dark:text-gray-400">
-                                Safari on iPhone
-                            </p>
-                        </div>
-                        <div class="inline-flex items-center">
-                            <a href="#"
-                                class="px-3 py-2 mb-3 mr-3 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Revoke</a>
-                        </div>
-                    </div>
-                </li>
+                <div v-show="sensor.observacoes == ''">
+                    Sem observações
+                </div>
             </ul>
-            <div>
+            <div v-show="sensor.observacoes != ''">
                 <button
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">See
                     more</button>
