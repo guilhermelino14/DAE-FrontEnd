f9c39c34fab08eabf2a3a44b5b67d5733aaf4f22
Leandro Filipe VIeira <leandro_vieira5@hotmail.com>
2023-12-23 23:40:04 +0000

    delete produto

--- pages/fabricante/produtos.vue
+++ pages/fabricante/produtos.vue
@@ -9,12 +9,12 @@
         <div class="items-center justify-between block sm:flex md:divide-x md:divide-gray-100 dark:divide-gray-700"
             style="justify-content: right;">
             <NuxtLink to="/fabricante/produtosCrud/new-produto">
-            <button
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button">
-                Adicionar Produto
-            </button>
-        </NuxtLink>
+                <button
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    type="button">
+                    Adicionar Produto
+                </button>
+            </NuxtLink>
         </div>
         <div class="flex flex-col mt-6">
             <div class="overflow-x-auto rounded-lg">
@@ -42,18 +42,19 @@
                                 </tr>
                             </thead>
                             <tbody class="bg-white dark:bg-gray-800">
-                                <tr v-for="produto in produtos">
+                                <tr v-for="(produto, index) in produtos">
                                     <td class="p-4 text-sm font-normal text-gray-900 whitespace-nowrap dark:text-white">
-                                        {{ produto.nome  }}
+                                        {{ produto.nome }}
                                     </td>
                                     <td class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                        {{ produto.categoria  }}
+                                        {{ produto.categoria }}
                                     </td>
                                     <td class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                        {{ produto.descricao  }}
+                                        {{ produto.descricao }}
                                     </td>
                                     <td class="p-4 whitespace-nowrap text-center">
-                                        <NuxtLink :to="`/fabricante/produtosCrud/${produto.id}`">
+                                        <NuxtLink :to="`/fabricante/produtosCrud/${produto.id}`"
+                                            :data-tooltip-target="index">
                                             <button type="button"
                                                 class="py-1.5 px-3 inline-flex items-center rounded-lg bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700">
                                                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
@@ -64,7 +65,14 @@
                                                 </svg>
                                             </button>
                                         </NuxtLink>
-                                        <button type="button"
+                                        <div :id="index" role="tooltip"
+                                            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                            Ver Detalhes
+                                            <div class="tooltip-arrow" data-popper-arrow></div>
+                                        </div>
+                                        <button type="button" :data-tooltip-target="index + 'eliminar'"
+                                            :data-modal-target="`delete-modal-` + produto.id"
+                                            :data-modal-toggle="`delete-modal-` + produto.id"
                                             class="ml-2 py-1.5 px-3 inline-flex items-center rounded-lg bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700">
                                             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
@@ -73,6 +81,15 @@
                                                     d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z" />
                                             </svg>
                                         </button>
+
+                                        <DeleteUserModal :message="'Tens a certeza de que deseja eliminar este sensor?'"
+                                            :id="produto.id" @confirmedDelete="deleteProduto" />
+
+                                        <div :id="index + 'eliminar'" role="tooltip"
+                                            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                            Eliminar Sensor
+                                            <div class="tooltip-arrow" data-popper-arrow></div>
+                                        </div>
                                     </td>
                                 </tr>
 
@@ -87,6 +104,7 @@
 <script setup>
 import { useAuthStore } from "~/store/auth-store.js"
 import { useToast } from 'vue-toastification'
+import DeleteUserModal from '~/components/delete-modal.vue'
 const authStore = useAuthStore()
 const toast = useToast()
 
@@ -99,4 +117,28 @@ definePageMeta({
     layout: 'fabricante',
     middleware: 'fabricante',
 })
+
+const deleteProduto = async (id) => {
+    try {
+        const response = await useFetch(`${api}/produtos/${id}`, {
+            method: 'DELETE',
+            headers: { "Authorization": `Bearer ${authStore.token}` },
+        });
+        if (response.status.value === "success") {
+            toast.success("Sucesso")
+            refresh()
+        }
+        if (response.status.value === "error") {
+            const errorObject = response.error._object[response.error._key];
+
+            if (errorObject && errorObject.data === "Produto has Produtos Fisicos.") {
+                toast.error("Erro: Produto possui Produtos Físicos.");
+            } else {
+                toast.error("Erro", response.error.value);
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 </script>
