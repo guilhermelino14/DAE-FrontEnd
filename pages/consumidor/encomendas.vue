a594eb877f4b5a9849e2fa326e5ce44de4377f6f
Leandro Filipe VIeira <leandro_vieira5@hotmail.com>
2023-12-29 01:42:19 +0000

    add encomendas details

--- pages/consumidor/encomendas.vue
+++ pages/consumidor/encomendas.vue
@@ -136,25 +136,29 @@
                                                 class="p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-white">
                                                 Status
                                             </th>
+                                            <th scope="col"
+                                                class="p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase dark:text-white">
+                                                Actions
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody class="bg-white dark:bg-gray-800">
-                                        <tr v-for="encomenda in encomendas">
+                                        <tr v-for="(encomenda, index) in encomendas">
                                             <td
                                                 class="p-4 text-sm font-normal text-gray-900 whitespace-nowrap dark:text-white">
                                                 Payment from <span class="font-semibold">Bonnie Green</span>
                                             </td>
                                             <td
                                                 class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
-                                                {{formatDate(encomenda.date)}}
+                                                {{ formatDate(encomenda.date) }}
                                             </td>
                                             <td
                                                 class="p-4 text-sm font-semibold text-gray-900 whitespace-nowrap dark:text-white">
                                                 $2300
                                             </td>
-                                            <td
-                                                class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400" style="text-align-last: center;">
-                                                {{encomenda.id}}
+                                            <td class="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400"
+                                                style="text-align-last: center;">
+                                                {{ encomenda.id }}
                                             </td>
                                             <td
                                                 class="inline-flex items-center p-4 space-x-2 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
@@ -180,9 +184,33 @@
                                                 <span>••• 475</span>
                                             </td>
                                             <td class="p-4 whitespace-nowrap">
-                                                    <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500" v-show="encomenda.status == 'ENTREGUE'">{{ encomenda.status }}</span>
-                                                    <span class="bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-orange-100 dark:bg-gray-700 dark:border-orange-300 dark:text-orange-300" v-show="encomenda.status == 'PENDENTE'">{{ encomenda.status }}</span>
-                                                    <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-red-100 dark:border-red-400 dark:bg-gray-700 dark:text-red-400" v-show="encomenda.status == 'CANCELADA'">{{ encomenda.status }}</span>
+                                                <span
+                                                    class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 border border-green-100 dark:border-green-500"
+                                                    v-show="encomenda.status == 'ENTREGUE'">{{ encomenda.status }}</span>
+                                                <span
+                                                    class="bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-orange-100 dark:bg-gray-700 dark:border-orange-300 dark:text-orange-300"
+                                                    v-show="encomenda.status == 'PENDENTE'">{{ encomenda.status }}</span>
+                                                <span
+                                                    class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-md border border-red-100 dark:border-red-400 dark:bg-gray-700 dark:text-red-400"
+                                                    v-show="encomenda.status == 'CANCELADA'">{{ encomenda.status }}</span>
+                                            </td>
+                                            <td class="p-4 whitespace-nowrap text-center">
+                                                <NuxtLink :to="`/consumidor/encomendasCrud/${encomenda.id}`">
+                                                    <button type="button" :data-tooltip-target="index"
+                                                        class="inline-flex items-center rounded-lg bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700">
+                                                        <svg class="w-6 h-6 text-gray-800 dark:text-white"
+                                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                            fill="currentColor" viewBox="0 0 20 14">
+                                                            <path
+                                                                d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+                                                        </svg>
+                                                    </button>
+                                                </NuxtLink>
+                                                <div :id="index" role="tooltip"
+                                                    class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                                    Ver Detalhes
+                                                    <div class="tooltip-arrow" data-popper-arrow></div>
+                                                </div>
                                             </td>
                                         </tr>
                                     </tbody>
