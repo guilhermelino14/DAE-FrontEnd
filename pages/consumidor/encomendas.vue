7fd815ddcb80422445a0b30050b0c71f5f58d35b
Guilherme Lino <lino000000@hotmail.com>
2023-12-29 00:14:58 +0000

    Update encomendas.vue

--- pages/consumidor/encomendas.vue
+++ pages/consumidor/encomendas.vue
@@ -1,5 +1,4 @@
 <template>
-    {{ encomendas }}
     <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800">
         <div class="flex flex-col mt-6">
             <div
