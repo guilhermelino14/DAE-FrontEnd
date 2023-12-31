e1dae372e6da5f945e75ae709cf3b5e1d2f7932f
Guilherme Lino <lino000000@hotmail.com>
2023-12-31 01:03:34 +0000

    Update cart-store.js

--- store/cart-store.js
+++ store/cart-store.js
@@ -1,13 +1,21 @@
 import { defineStore } from "pinia";
 import { useStorage } from '@vueuse/core'
 import { ref, computed } from "vue"
+import { useToast } from 'vue-toastification'
+const toast = useToast()
 
 export const useCartStore = defineStore('cart', () => {
     const cart = useStorage('cart', ref([]))
     const total = useStorage('total', ref(0))
 
     function addToCart(item) {
+        const exists = cart.value.find(i => i.id === item.id)
+        if (exists) {
+            toast.error("Já existe no carrinho")
+            return
+        }
         cart.value.push(item)
+        toast.success("Adicionado ao carrinho")
     }
 
     function removeFromCart(index) {
