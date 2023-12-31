55fbc65faa3d9748f3773d305c1d4b47c9891afb
Guilherme Lino <lino000000@hotmail.com>
2023-12-31 16:58:48 +0000

    update

--- pages/operador/encomendasCrud/[id].vue
+++ pages/operador/encomendasCrud/[id].vue
@@ -1,7 +1,7 @@
 <template>
     <div
         class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 sm:p-6 dark:bg-gray-800 pb-2 mb-3">
-        <NuxtLink to="/fabricante/encomendas">
+        <NuxtLink to="/operador/encomendas">
             <button type="button"
                 class="mb-2 py-1.5 px-3 inline-flex items-center rounded-lg bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600 dark:bg-gray-700">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
