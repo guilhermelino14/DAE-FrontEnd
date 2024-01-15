4d8c828a11d22c0dc7b4c9203352d74c8fdbf8a7
Guilherme Lino <lino000000@hotmail.com>
2024-01-15 17:21:39 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -15,12 +15,9 @@
                 </div>
                 <div class="flex items-center justify-between">
                     <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a href="#" @click="addToCart(produto)" v-show="produto.stock> 0"
+                    <a href="#" @click="addToCart(produto)"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                        to cart</a>
-                    <a href="#" v-show="produto.stock <= 0" disabled
-                        class="cursor-not-allowed text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Sem Stock</a>
-                </div>
+                        to cart</a></div>
             </div>
         </div>
 
