3c9f7fde17bef47d56d95bb79f9bdc52d545576d
Guilherme Lino <lino000000@hotmail.com>
2024-01-16 18:00:17 +0000

    update

--- layouts/operador.vue
+++ layouts/operador.vue
@@ -2,8 +2,7 @@
     <div>
       <NavBar></NavBar>
       <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
-        <Sidebar></Sidebar>
-        <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+        <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50  dark:bg-gray-900">
           <main style="height: 100vh;">
             <div class="px-4 pt-6">
               <slot />
@@ -18,7 +17,6 @@
   <script setup>
   import { onMounted } from 'vue'
   import { initFlowbite } from 'flowbite'
-  import Sidebar from '../components/operadorLayout/sidebar.vue'
   import NavBar from '../components/operadorLayout/topBar.vue'
   onMounted(() => {
     initFlowbite();
