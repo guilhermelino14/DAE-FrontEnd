b165a3bc278a8cde11296726859a899fe6afe295
Leandro Filipe VIeira <leandro_vieira5@hotmail.com>
2024-01-18 23:59:32 +0000

    fix add produto

--- pages/fabricante/produtosCrud/[id].vue
+++ pages/fabricante/produtosCrud/[id].vue
@@ -34,6 +34,16 @@
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Chocolate branco ....." required></textarea>
             </div>
+            <div class="col-span-12">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantidade</label>
+                <input type="text" name="nome" v-model="produto.quantidade" disabled
+                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+            </div>
+            <div v-if="produto.typeOfSensor" class="col-span-12">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo do sensor</label>
+                <input type="text" name="nome" v-model="produto.typeOfSensor" disabled
+                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+            </div>
             <!-- <div class="col-span-12 flex  items-end">
                 <div>
                     <label for="produtos-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
--- pages/fabricante/produtosCrud/new-produto.vue
+++ pages/fabricante/produtosCrud/new-produto.vue
@@ -42,6 +42,30 @@
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Chocolate branco ....." required></textarea>
             </div>
+            <div class="mb-5">
+                <label for="base-input"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantidade</label>
+                <input type="text" v-model="newProduto.quantidade" placeholder="Comida" required
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+            <div class="flex items-center mb-4">
+                <input id="default-checkbox" type="checkbox" :value="sensorOnEmbalagemEncomenda"
+                    v-on:change="sensorOnEmbalagemEncomenda = !sensorOnEmbalagemEncomenda"
+                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Adicionar
+                    sensor a
+                    embalagem de transporte ?</label>
+            </div>
+            <div v-if="sensorOnEmbalagemEncomenda">
+                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecionar o
+                    tipo de
+                    Sensor</label>
+                <select id="countries" v-model="newProduto.typeOfSensor"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="TEMPERATURA">TEMPERATURA</option>
+                    <option value="HUMIDADE">HUMIDADE</option>
+                </select>
+            </div>
             <div style="text-align: right;">
                 <button :disable="loading" type="submit"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -75,16 +99,20 @@ const emit = defineEmits(['refresh'])
 const toast = useToast()
 const config = useRuntimeConfig()
 const api = config.public.API_URL
+const sensorOnEmbalagemEncomenda = ref(false)
 
 const newProduto = ref({
     nome: '',
     categoria: '',
-    descricao: ''
+    descricao: '',
+    quantidade: 0,
+    typeOfSensor: ''
 })
 const loading = ref(false)
 
 const adicionar = async () => {
     loading.value = true
+    if (!sensorOnEmbalagemEncomenda.value) newProduto.value.typeOfSensor = null
     try {
         const response = await useFetch(`${api}/produtos/`, {
             method: 'POST',
