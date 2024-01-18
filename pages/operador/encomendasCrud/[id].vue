4eff3f76c0f02f14f28217920cbdef5fdbf86e93
Guilherme Lino <lino000000@hotmail.com>
2024-01-18 23:31:47 +0000

    Update [id].vue

--- pages/operador/encomendasCrud/[id].vue
+++ pages/operador/encomendasCrud/[id].vue
@@ -43,7 +43,7 @@
                     Encomenda</button>
 
                 <button v-if="encomenda.status != 'ENTREGUE' && encomenda.status == 'EM_TRANSITO'"
-                    @click="" type="button"
+                    @click="tentativaDeEntrega(encomenda.id)" type="button"
                     class="h-full w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Tentativa de entregar encomenda falhada</button>
             </div>
             <div class="col-span-12 md:col-span-6 p-1"
@@ -251,6 +251,17 @@ const confirmarEncomenda = async (id) => {
     })
     if (data) {
         toast.success('Encomenda confirmada com sucesso!')
+        refresh()
+    }
+}
+
+const tentativaDeEntrega = async (id) => {
+    const { data, error } = await useFetch(`${api}/encomendas/${id}/status/tentativaEntrega`, {
+        method: 'POST',
+        headers: { "Authorization": `Bearer ${authStore.token}` }
+    })
+    if (data) {
+        toast.success('Tentativa de entrega registada com sucesso!')
         refresh()
     }
 }
