48df86e87e01f861857740271f6ba91e50151ce2
Guilherme Lino <lino000000@hotmail.com>
2024-01-18 22:11:16 +0000

    Update testes.vue

--- pages/testes.vue
+++ pages/testes.vue
@@ -100,7 +100,7 @@
                                 </div>
 
                             </div>
-                            <div>
+                            <div style="    text-align: right;">
                                 <button @click="createObservacao()"
                                     class="px-5 py-2.5 mt-5 text-sm font-medium text-center inline-flex items-center text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                     <svg class="w-4 h-4 me-2 -ms-1 text-gray-800 dark:text-white" aria-hidden="true"
