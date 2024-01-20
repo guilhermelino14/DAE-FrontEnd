d4ab6ce0a2b90d37a379475ccfddec0d77ed975c
Guilherme Lino <lino000000@hotmail.com>
2024-01-20 14:54:30 +0000

    update

--- pages/fabricante/produtosCrud/[id].vue
+++ pages/fabricante/produtosCrud/[id].vue
@@ -27,7 +27,11 @@
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
                 <input type="text" name="nome" v-model="produto.categoria" disabled
                     class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                {{ isLiquid(produto.liquido) }}
+            </div>
+            <div class="col-span-12" v-if="produto.liquido">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipo de produto</label>
+                <input type="text" name="nome" :value="isLiquid(produto.liquido)" disabled
+                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
             </div>
             <div class="col-span-12">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descricao</label>
